5a039b4c768312ad57eefeeafaa9e5f3bef57f2e
Jhanvi12 <36741795+Jhanvi12@users.noreply.github.com>
2022-05-18 17:39:05 +0530

    Updated vulnerable TF folder

--- VulnerableTF/resource-wc.tf
+++ VulnerableTF/resource-wc.tf
@@ -1,10 +1,21 @@
 resource "aws_iam_policy" "res-1-fail" {
    
-   policy = 
- {
-   "Version": "2012-10-17",
-   "Statement": {
-     "Effect": "Allow",
-     "Action": "Configured",
-     "Resource": "*"
-   }
+   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "Configured" ,
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+
+
+resource "aws_internet_gateway" "gateway-1"{
+  
+}
