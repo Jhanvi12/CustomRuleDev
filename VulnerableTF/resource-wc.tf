73357aa0f297012d60e08ae96dbb2cabe80281b5
Jhanvi12 <36741795+Jhanvi12@users.noreply.github.com>
2022-05-25 15:00:13 +0530

    Added 3 vulnerable TF
    Added 3 vulnerable TF for raising 'Resource:*'

--- VulnerableTF/resource-wc.tf
+++ VulnerableTF/resource-wc.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_policy" "res-1-fail" {
+resource "aws_iam_policy" "res-1-fail-type1" {
    
    policy = <<EOF
 {
@@ -14,8 +14,52 @@ resource "aws_iam_policy" "res-1-fail" {
 EOF
 }
 
-
+resource "aws_iam_policy" "res-1-fail-type2" {
+   
+   policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": 
+    {
+      "Action": "Configured" ,
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  
+}
+EOF
+}
 
 resource "aws_internet_gateway" "gateway-1"{
   
+}
+
+
+
+resource "aws_iam_policy" "policy" {
+  name        = "${random_pet.pet_name.id}-policy"
+  description = "My test policy"
+
+  policy = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "s3:ListAllMyBuckets"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "s3:*"
+      ],
+      "Effect": "Allow",
+      "Resource": "${aws_s3_bucket.bucket.arn}"
+    }
+  ]
+
+}
+EOT
 }
